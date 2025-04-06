f7c73acf1777ee89d00d21a07ce2297beb8debe5
TAKSH ASHISH GANDHI <takshgandhi4@gmail.com>
2025-04-06 18:03:36 +0530

    Update firebaseauth.js

--- Team Codequest/Javasxript/firebaseauth.js
+++ Team Codequest/Javasxript/firebaseauth.js
@@ -4,14 +4,16 @@ import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } f
 import { getFirestore, setDoc, doc } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAjsaWoMjU3YquZUxsoH_HK6F6YMIIPJ7s",
-  authDomain: "login-ec6b5.firebaseapp.com",
-  projectId: "login-ec6b5",
-  storageBucket: "login-ec6b5.appspot.com",
-  messagingSenderId: "779274933126",
-  appId: "1:779274933126:web:787373132a74f73cbb1207"
+  const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
+
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
